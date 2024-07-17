7adc4c550c62ec66b96cc70b3cb7fc4a4c86c2ed
Meherab <mejerabxd@gmail.com>
2024-07-17 13:58:38 -0700

    solving problems

--- index.js
+++ index.js
@@ -19,66 +19,7 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
 
-        const craftsCollection = client.db('Art&CraftDB').collection('crafts');
-        app.get('/crafts', async (req, res) => {
-            const cursor = craftsCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
-        app.post('/crafts', async (req, res) => {
-            const newCraft = req.body;
-            console.log(newCraft);
-            const result = await craftsCollection.insertOne(newCraft);
-            res.send(result);
-        })
-        app.get('/crafts/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await craftsCollection.findOne(query);
-            res.send(result);
-        })
-        app.put('/crafts/:id', async(req, res)=>{
-            const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
-            const options = {upsert: true};
-            const craft = req.body;
-            const craftList = {
-                $set: {
-                    artistName : craft.artistName,
-                    artistEmail : craft.artistEmail,
-                    name : craft.name,
-                    photo : craft.photo,
-                    subcategory : craft.subcategory,
-                    description : craft.description,
-                    price : craft.price,
-                    rating : craft.rating,
-                    stock : craft.stock,
-                    time : craft.time,
-                    customizer: craft.customizer
-                }
-            }
-            const result = await craftsCollection.updateOne(filter, craftList, options);
-            res.send(result);
-        })
-        app.delete('/crafts/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await craftsCollection.deleteOne(query);
-            res.send(result);
-        })
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
-}
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
